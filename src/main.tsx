84f428b3322576720363723a5fbc726c72e8f226
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-07-31 22:39:48 -0300

    chore: Update landing page banner color and remove unused CSS styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
+import './style/index.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+
+import { App } from './app'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
