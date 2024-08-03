2fbc303682753adee9e66f18aa4678a700575ff8
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-08-03 16:14:03 -0300

    chore: Update vite.config.ts to include TanStackRouterVite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 /// <reference types="vitest" />
 import path from 'node:path'
 
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+// vitest automatically sets NODE_ENV to 'test' when running tests
+const isTest = process.env.NODE_ENV === 'test'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [!isTest && TanStackRouterVite(), react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
