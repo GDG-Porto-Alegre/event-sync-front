a46ff2b87b407480abbb684609423072994e34c0
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-08-03 16:15:39 -0300

    chore: Update main.tsx to use TanStack Router for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,26 @@
 import './style/index.css'
 
+import { createRouter, RouterProvider } from '@tanstack/react-router'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { App } from './app'
+import { routeTree } from './route-tree.gen'
+
+// Set up a Router instance
+const router = createRouter({
+  routeTree,
+  defaultPreload: 'intent',
+})
+
+// Register things for typesafety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
